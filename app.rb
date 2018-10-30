30573f0a9aa6c246ed2de91bbbcf341a6407a7f1
yoshinator <yoanante@gmail.com>
2018-10-29 22:23:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
+  post '/' do   
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
